ab0a7b731cf021f3d0182a85773e2ac1495ed02b
Lauren Rosenberg <t-laro@microsoft.com>
2019-07-15 09:38:59 -0700

    Minor test fixes; VerifyEmitDiag -> VerifyDiag, indentations

--- src/Compilers/CSharp/Test/Semantic/Semantics/NullCheckedParameterTests.cs
+++ src/Compilers/CSharp/Test/Semantic/Semantics/NullCheckedParameterTests.cs
@@ -799,5 +799,23 @@ class Program
                     //     static void Main(string[] args!) { }
                     Diagnostic(ErrorCode.ERR_MissingPredefinedMember, "args").WithArguments("System.ArgumentNullException", ".ctor").WithLocation(4, 31));
         }
+
+        [Fact]
+        public void TestNullCheckedWithMissingType()
+        {
+            var source =
+@"
+class Program
+{
+    static void Main(string[] args!) { }
+}";
+            var comp = CreateCompilation(source);
+            comp.MakeMemberMissing(WellKnownMember.System_ArgumentNullException__ctorString);
+            comp.MakeTypeMissing(WellKnownType.System_ArgumentNullException);
+            comp.VerifyDiagnostics(
+                    // (4,31): error CS0656: Missing compiler required member 'System.ArgumentNullException..ctor'
+                    //     static void Main(string[] args!) { }
+                    Diagnostic(ErrorCode.ERR_MissingPredefinedMember, "args").WithArguments("System.ArgumentNullException", ".ctor").WithLocation(4, 31));
+        }
     }
 }
