377c09d405853708277be946bd12b80cda538b34
Lauren Rosenberg <t-laro@microsoft.com>
2019-07-15 09:46:30 -0700

    Removed unnecessary using statement in test

--- src/Compilers/CSharp/Test/Emit/CodeGen/CodeGenNullCheckedParameterTests.cs
+++ src/Compilers/CSharp/Test/Emit/CodeGen/CodeGenNullCheckedParameterTests.cs
@@ -1425,7 +1425,6 @@ System.ArgumentNullException");
         {
             var source =
 @"
-using System;
 class Program
 {
     public static void Main()
@@ -1504,9 +1503,9 @@ class Program
             comp.MakeMemberMissing(WellKnownMember.System_ArgumentNullException__ctorString);
             comp.MakeTypeMissing(WellKnownType.System_ArgumentNullException);
             comp.VerifyDiagnostics(
-                    // (8,23): error CS0656: Missing compiler required member 'System.ArgumentNullException..ctor'
+                    // (7,23): error CS0656: Missing compiler required member 'System.ArgumentNullException..ctor'
                     //         void M(string x!) { }
-                    Diagnostic(ErrorCode.ERR_MissingPredefinedMember, "x").WithArguments("System.ArgumentNullException", ".ctor").WithLocation(8, 23));
+                    Diagnostic(ErrorCode.ERR_MissingPredefinedMember, "x").WithArguments("System.ArgumentNullException", ".ctor").WithLocation(7, 23));
         }
 
         [Fact]
