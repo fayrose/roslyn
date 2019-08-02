4d62a1eca3bfa22777690b34428fd102409f18a4
Lauren Rosenberg <t-laro@microsoft.com>
2019-08-02 15:03:35 -0700

    Added WellKnownMember implementation (move to own file)

--- src/Compilers/CSharp/Portable/Symbols/Synthesized/SynthesizedStringHashFunctionSymbol.cs
+++ src/Compilers/CSharp/Portable/Symbols/Synthesized/SynthesizedStringHashFunctionSymbol.cs
@@ -17,4 +17,27 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
             this.SetParameters(ImmutableArray.Create<ParameterSymbol>(SynthesizedParameterSymbol.Create(this, TypeWithAnnotations.Create(paramType), 0, RefKind.None, "s")));
         }
     }
+
+    /// <summary>
+    /// Represents a compiler generated synthesized method symbol
+    /// representing string switch hash function
+    /// </summary>
+    internal sealed partial class ThrowIfNullMethod : SynthesizedGlobalMethodSymbol
+    {
+        internal ThrowIfNullMethod(SourceModuleSymbol containingModule, PrivateImplementationDetails privateImplType, TypeSymbol returnType, TypeSymbol paramType)
+            : base(containingModule, privateImplType, returnType, WellKnownMemberNames.ThrowIfNullMethodName)
+        {
+            this.SetParameters(ImmutableArray.Create<ParameterSymbol>(SynthesizedParameterSymbol.Create(this, TypeWithAnnotations.Create(paramType), 0, RefKind.None, name: "name")));
+        }
+
+        internal override void GenerateMethodBody(TypeCompilationState compilationState, DiagnosticBag diagnostics)
+        {
+            var factory = new SyntheticBoundNodeFactory(this, this.GetNonNullSyntaxNode(), compilationState, diagnostics);
+            factory.CurrentFunction = this;
+            var argumentName = ImmutableArray.Create<BoundExpression>(factory.StringLiteral(this.Parameters[0].Name));
+            BoundObjectCreationExpression ex = factory.New(factory.WellKnownMethod(WellKnownMember.System_ArgumentNullException__ctorString), argumentName);
+            var throwArgNullStatement = factory.Throw(ex);
+            factory.CloseMethod(throwArgNullStatement);
+        }
+    }
 }
